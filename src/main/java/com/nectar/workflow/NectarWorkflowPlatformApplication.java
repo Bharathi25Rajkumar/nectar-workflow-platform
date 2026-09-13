0a921bfd9da9b2a2c5e18682cc7feb507b6c2bef
Bharathi Rajkumar <bharathi25rajkumar@gmail.com>
2026-09-13 13:05:44 +0530

    initial commit

---
+++ src/main/java/com/nectar/workflow/NectarWorkflowPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.nectar.workflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NectarWorkflowPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NectarWorkflowPlatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nectar/workflow/NectarWorkflowPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nectar.workflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NectarWorkflowPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
